7f3700d59408c83a8b94143b41ff2669408c34ce
gwizardkyle <250549961@qq.com>
2020-05-16 21:15:08 +0800

    789456

--- src/test/java/com/iiaccout/yiguan/OpenYg.java
+++ src/test/java/com/iiaccout/yiguan/OpenYg.java
@@ -18,7 +18,7 @@ import java.util.Map;
 import static com.iiaccount.asserts.Asserts.asserts;
 
 /*
- *一贯开立II/III类户******************
+ *一贯开立II/III类户*********************
  * 环境参数在SetUpTearDown 父类定义***
  */
 @Feature("分类账户改造")
