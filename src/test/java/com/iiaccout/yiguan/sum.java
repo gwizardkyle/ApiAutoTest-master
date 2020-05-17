356368dca18bd1e2fcd162417981078984f5a958
gwizardkyle <250549961@qq.com>
2020-05-17 22:09:16 +0800

    提交了一大堆东西

---
+++ src/test/java/com/iiaccout/yiguan/sum.java
@@ -0,0 +1,27 @@
+package com.iiaccout.yiguan;
+
+import io.qameta.allure.Feature;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class sum {
+    @Feature("算数计算功能验证")
+    @BeforeClass //只运行一次
+    public void beforeClass() {
+        System.out.println("this is before class");
+    }
+
+    @Test
+    public void TestNgLearn() {
+        System.out.println("this is TestNG test case");
+    }
+
+    @AfterClass //只运行一次
+    public void afterClass() {
+        System.out.println("this is after class");
+    }
+}
+
+
+
